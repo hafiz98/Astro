52d520cb81b291f9fe28adb18748cf2aaca3a98e
Hafiz <elkilic.h@hotmail.com>
2018-03-28 16:31:15 +0200

    .

--- Astro/Astro.ino
+++ Astro/Astro.ino
@@ -11,7 +11,8 @@ const int aiVrYHelkBout = A1;
 SimpleTimer timer;
 
 
-int timer1;
+int gamemode = 1;
+bool start = 0;
 
 void setup() {
   pinMode(diSwHelkBout, INPUT);
@@ -22,24 +23,28 @@ void setup() {
   lcd.begin();
 
   // Turn on the blacklight and print a message.
-  lcd.backlight(); 
+  lcd.backlight();
+timer.run();
 
-<<<<<<< HEAD
-splashScreen();
- 
-=======
->>>>>>> 58815777651df1bfb60e0765fc78f94a7c0cb739
 }
 
 void loop() {
-splashScreen();
-
-
-<<<<<<< HEAD
- // joystickRemote();
-=======
->>>>>>> 58815777651df1bfb60e0765fc78f94a7c0cb739
-
+  switch (gamemode) {
+    case 1:
+      Serial.println("SPLASHSCREEN");
+      if( start == 0){
+        
+        timer.setTimer(3000, splashScreen, 5);
+        gamemode = 2;
+      }
+      start = 1;
+      break;
+    case 2:
+      Serial.println("SPEL");
+      joystickRemote();
+      break;
+  }
+  
   if (Serial.available()) {
     // Wait a bit for the entire message to arrive
     delay(100);
@@ -51,28 +56,28 @@ splashScreen();
       lcd.write(Serial.read());
     }
   }
+  
 }
 
-void joystickRemote(){
-  lcd.clear();
-  if(analogRead(aiVrXHelkBout) < 250){
+void joystickRemote() {
+  if (analogRead(aiVrXHelkBout) < 250) {
     Serial.println("Ik moet naar boven");
     clearJoystickPos();
     joystickTop();
   }
-  if(analogRead(aiVrXHelkBout) > 750 && analogRead(aiVrXHelkBout) < 1000){
+  if (analogRead(aiVrXHelkBout) > 750 && analogRead(aiVrXHelkBout) < 1000) {
     Serial.println("Ik moet naar beneden");
     clearJoystickPos();
     joystickBot();
   }
-  if(analogRead(aiVrXHelkBout) > 400 && analogRead(aiVrXHelkBout) < 600){
+  if (analogRead(aiVrXHelkBout) > 400 && analogRead(aiVrXHelkBout) < 600) {
     Serial.println("Ik moet naar midden");
     clearJoystickPos();
     joystickMid();
   }
 }
 
-void clearJoystickPos(){
+void clearJoystickPos() {
   lcd.setCursor(0, 0);
   lcd.print(" ");
 
@@ -83,32 +88,27 @@ void clearJoystickPos(){
   lcd.print(" ");
 }
 
-void joystickTop(){
+void joystickTop() {
   lcd.setCursor(0, 0);
   lcd.print("(");
 }
-void joystickMid(){
+void joystickMid() {
   lcd.setCursor(0, 1);
   lcd.print("(");
 }
-void joystickBot(){
+void joystickBot() {
   lcd.setCursor(0, 2);
   lcd.print("(");
 }
 
 
 
-void splashScreen(){
-  timer.run();
-  timer1 = timer.setTimer(5000, joystickRemote, 1);
-
-  if(timer.isEnabled(timer1)) {
-     lcd.clear();
-  }
+void splashScreen() {
+ 
 
   lcd.setCursor(0, 0);
   lcd.print("Astroidinator V1");
- 
+
   lcd.setCursor(0, 1);
   lcd.print("Gemaakt door:");
 
@@ -117,6 +117,20 @@ void splashScreen(){
 
   lcd.setCursor(0, 3);
   lcd.print("Bram");
+  
+  
 }
 
+void menu()
+{
+  lcd.setCursor(0, 0);
+  lcd.print("Main menu");
+
+  lcd.setCursor(0, 1);
+  lcd.print("Start");
+  lcd.setCursor(0, 2);
+  lcd.print("Credits");
+
+
+}
 
