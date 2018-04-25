d5078ce1448c1f742664545969f7aca4819be158
Hafiz <elkilic.h@hotmail.com>
2018-04-25 18:24:01 +0200

    UPDATE
    Rockets ga ik maken
    
    comment mag jij toevoegen
    geluid schieten maken (als je da kan maken zou cool zijn)
    init functie die van meneer weetje wel anders app mij

--- Astro/Astro.ino
+++ Astro/Astro.ino
@@ -1,82 +1,136 @@
 #include <SimpleTimer.h>
 #include <LiquidCrystal_I2C.h>
 
+//draden goed fixen
+//commentaar wanneer gebruiken?
+//rockets
+//commentaar code
+//na schieten geluid
+//init functie gebruiken
 
 // Set the LCD address to 0x27 for a 16 chars and 2 line display
-LiquidCrystal_I2C lcd(0x27, 16, 3);
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 const int diSwHelkBout = 2;
 const int aiVrXHelkBout = A0;
 const int aiVrYHelkBout = A1;
-SimpleTimer timer;
+const int doBuzzerHelkBout = 3;
+SimpleTimer timerGenerateAstroids;
+SimpleTimer timerMoveAstroids;
+SimpleTimer timerGenerateSpaceship;
+SimpleTimer timerMoveSpaceship;
+SimpleTimer timerSplashScreen;
+SimpleTimer timerGameOverScreen;
 
 
-int gamemode = 1;
-bool start = 0;
+typedef struct {
+  int xPosition;
+  int yPosition;
+  int randomXPositionHelkBout = random(20, 100);
+  int randomYPositionHelkBout = random(0, 2);
+  int number;
+  String symbolUfo;
+} ufo;
+
+ufo Astroid[5];
+ufo Spaceship[5];
+
+int joystickYPositionHelkBout;
+int joystickXPositionHelkBout;
+int scoreAstroidHelkBout = 0;
+int scoreSpaceshipHelkBout = 10;
+int gameStatus = 0;
 
 void setup() {
   pinMode(diSwHelkBout, INPUT);
   digitalWrite(diSwHelkBout, HIGH);
   Serial.begin(9600);
-
-  // initialize the LCD
   lcd.begin();
-
-  // Turn on the blacklight and print a message.
   lcd.backlight();
-timer.run();
+  
+  timerGenerateAstroids.setTimer(1000, generateAstroids, 1); // dit moet in functie
+  timerMoveAstroids.setInterval(1000, moveAstroids);
 
+  timerGenerateSpaceship.setTimer(1000, generateSpaceship, 1);
+  timerMoveSpaceship.setInterval(1000, moveSpaceship);
+
+  timerSplashScreen.setInterval(1000, splashScreen);
+  timerGameOverScreen.setInterval(1000, gameOverScreen);
 }
 
 void loop() {
-  switch (gamemode) {
-    case 1:
-      Serial.println("SPLASHSCREEN");
-      if( start == 0){
-        
-        timer.setTimer(3000, splashScreen, 5);
-        gamemode = 2;
-      }
-      start = 1;
-      break;
-    case 2:
-      Serial.println("SPEL");
-      joystickRemote();
-      break;
+
+  if (gameStatus == 0) {
+    Serial.print("gamestatus: ");
+    Serial.println(gameStatus);
+    timerSplashScreen.run();
+  }
+
+  if (gameStatus == 1) {
+    scoreBoard();
+    joystickRemote();
+    timerGenerateAstroids.run();
+    timerMoveAstroids.run();
+    timerGenerateSpaceship.run();
+    timerMoveSpaceship.run();
   }
   
-  if (Serial.available()) {
-    // Wait a bit for the entire message to arrive
-    delay(100);
-    // Clear the screen
-    lcd.clear();
+//  if (gameStatus == 3) {
+//    Serial.print("gamestatus: ");
+//    Serial.println(gameStatus);
+//    timerSplashScreen.run();
+//  }
 
-    // Write all characters received with the serial port to the LCD.
+  if (scoreSpaceshipHelkBout == 0) {
+    gameOverScreen();
+  }
+  //splashScreen();
+
+  //  if (gameStatus == 0) {
+  //    Serial.print("gamestatus: ");
+  //    Serial.println(gameStatus);
+  //    timerSplashScreen.run();
+  //  }
+
+  //  if (gameStatus == 1){
+  //    Serial.print("gamestatus: ");
+  //    Serial.println(gameStatus);
+  //    scoreBoard();
+  //    joystickRemote();
+  //    timerGenerateAstroids.run();
+  //    timerMoveAstroids.run();
+  //    timerGenerateSpaceship.run();
+  //    timerMoveSpaceship.run();
+  //  }
+
+
+
+
+  if (Serial.available()) {
+    delay(100);    // Wait a bit for the entire message to arrive
+    lcd.clear();    // Clear the screen
     while (Serial.available() > 0) {
       lcd.write(Serial.read());
     }
   }
-  
+
 }
 
 void joystickRemote() {
+
   if (analogRead(aiVrXHelkBout) < 250) {
-    Serial.println("Ik moet naar boven");
     clearJoystickPos();
     joystickTop();
   }
   if (analogRead(aiVrXHelkBout) > 750 && analogRead(aiVrXHelkBout) < 1000) {
-    Serial.println("Ik moet naar beneden");
     clearJoystickPos();
     joystickBot();
   }
   if (analogRead(aiVrXHelkBout) > 400 && analogRead(aiVrXHelkBout) < 600) {
-    Serial.println("Ik moet naar midden");
     clearJoystickPos();
     joystickMid();
   }
 }
-
 void clearJoystickPos() {
   lcd.setCursor(0, 0);
   lcd.print(" ");
@@ -87,25 +141,49 @@ void clearJoystickPos() {
   lcd.setCursor(0, 2);
   lcd.print(" ");
 }
-
 void joystickTop() {
+  joystickYPositionHelkBout = 0;
+  joystickXPositionHelkBout = 0;
   lcd.setCursor(0, 0);
   lcd.print("(");
 }
 void joystickMid() {
+  joystickYPositionHelkBout = 1;
+  joystickXPositionHelkBout = 0;
   lcd.setCursor(0, 1);
   lcd.print("(");
 }
 void joystickBot() {
+  joystickYPositionHelkBout = 2;
+  joystickXPositionHelkBout = 0;
   lcd.setCursor(0, 2);
   lcd.print("(");
 }
+void scoreBoard() {
+  lcd.setCursor(0, 3);
+  lcd.print("S=");
 
+  lcd.setCursor(2, 3);
+  lcd.print(scoreSpaceshipHelkBout);
 
+  lcd.setCursor(5, 3);
+  lcd.print("A=");
 
+  lcd.setCursor(7, 3);
+  lcd.print(scoreAstroidHelkBout);
+}
+void buzzerAstroid() {
+  tone(doBuzzerHelkBout, 1000, 250);
+}
+void buzzerSpaceship() {
+  tone(doBuzzerHelkBout, 1000, 250);
+  delay(250);
+  tone(doBuzzerHelkBout, 1000, 250);
+  delay(250);
+  tone(doBuzzerHelkBout, 100, 1000);
+}
 void splashScreen() {
- 
-
+  Serial.println("ik ben in splash screen!");
   lcd.setCursor(0, 0);
   lcd.print("Astroidinator V1");
 
@@ -116,21 +194,94 @@ void splashScreen() {
   lcd.print("Hafiz");
 
   lcd.setCursor(0, 3);
-  lcd.print("Bram");
-  
-  
+  lcd.print("Nassim");
+
+  gameStatus = 1;
+  Serial.print("gamestatus: ");
+  Serial.println(gameStatus);
+
+  delay(2500);
+  lcd.clear();
+}
+void gameOverScreen() {
+  Serial.println("ik ben in gameover screen!");
+  lcd.setCursor(5, 1);
+  lcd.print("Game  over");
+
+  lcd.setCursor(6, 2);
+  lcd.print("Astroids");
+
+  lcd.setCursor(9, 3);
+  lcd.print(scoreAstroidHelkBout);
+  gameStatus = 3;
+
+  delay(2500);
+  lcd.clear();
 }
 
-void menu()
-{
-  lcd.setCursor(0, 0);
-  lcd.print("Main menu");
-
-  lcd.setCursor(0, 1);
-  lcd.print("Start");
-  lcd.setCursor(0, 2);
-  lcd.print("Credits");
-
-
+void generateAstroids() {
+  for (int i = 0; i < 5; i++) {
+    Astroid[i].xPosition = Astroid[i].randomXPositionHelkBout;
+    if (Astroid[i].xPosition % 2) {
+      Astroid[i].xPosition + 1;
+    }
+    Astroid[i].yPosition = Astroid[i].randomYPositionHelkBout;
+    lcd.setCursor(Astroid[i].xPosition, Astroid[i].yPosition);
+    lcd.print("*");
+  }
+}
+void moveAstroids() {
+  for (int i = 0; i < 5; i++) {
+    lcd.setCursor(Astroid[i].xPosition, Astroid[i].yPosition);
+    lcd.print(" ");
+    Astroid[i].xPosition = Astroid[i].xPosition - 1;
+    if (joystickYPositionHelkBout == Astroid[i].yPosition && joystickXPositionHelkBout == Astroid[i].xPosition) {
+      Astroid[i].xPosition = 35;
+      buzzerAstroid();
+      scoreAstroidHelkBout = scoreAstroidHelkBout + 1;
+    }
+    if (Astroid[i].xPosition >= 0 && Astroid[i].xPosition <= 20) {
+      lcd.setCursor(Astroid[i].xPosition, Astroid[i].yPosition);
+      lcd.print("*");
+    }
+  }
 }
 
+void generateSpaceship() {
+  Serial.println("-----HIERONDER GEGENEREEDE Spaceship-------");
+  for (int i = 0; i < 4; i++)
+  {
+    Spaceship[i].xPosition = Spaceship[i].randomXPositionHelkBout;
+    if (Spaceship[i].xPosition % 2) 
+    {
+      Spaceship[i].xPosition + 1;
+    }
+    Spaceship[i].yPosition = Spaceship[i].randomYPositionHelkBout;
+    lcd.setCursor(Spaceship[i].xPosition, Spaceship[i].yPosition);
+    lcd.print("<");
+  }
+}
+
+void moveSpaceship() {
+  Serial.println("-----HIERONDER NIEUWE POSITIE-------");
+  for (int i = 0; i < 4; i++) {
+    lcd.setCursor(Spaceship[i].xPosition, Spaceship[i].yPosition);
+    lcd.print(" ");
+    Spaceship[i].xPosition = Spaceship[i].xPosition - 1;
+    Serial.print("xpos van spaceship: ");
+    Serial.println(Spaceship[i].xPosition);
+    if (joystickYPositionHelkBout == Spaceship[i].yPosition && joystickXPositionHelkBout == Spaceship[i].xPosition) {
+      Spaceship[i].xPosition = 35;
+      buzzerSpaceship();
+      scoreSpaceshipHelkBout = scoreSpaceshipHelkBout - 1;
+      lcd.noBacklight();
+      delay(150);
+      lcd.backlight();
+    }
+    if (Spaceship[i].xPosition >= 0 && Spaceship[i].xPosition <= 20) {
+      Serial.println(scoreSpaceshipHelkBout);
+      lcd.setCursor(Spaceship[i].xPosition, Spaceship[i].yPosition);
+      lcd.print("<");
+    }
+  }
+}
